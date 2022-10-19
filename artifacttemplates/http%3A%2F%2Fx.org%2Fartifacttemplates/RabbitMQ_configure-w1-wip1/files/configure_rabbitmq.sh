bd3457563bce2d70b06c33c8ed48dfffee95df05
Jan Fuhrmann <janfuhrmann1@live.de>
2022-10-19 14:26:29 +0200

    Delete artifacttemplates/http%3A%2F%2Fx.org%2Fartifacttemplates/RabbitMQ_configure-w1-wip1 directory

--- artifacttemplates/http%3A%2F%2Fx.org%2Fartifacttemplates/RabbitMQ_configure-w1-wip1/files/configure_rabbitmq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "RabbitMQ: Create User $User with password $Password and grant privileges to allow remote connections"
-rabbitmqctl add_user $User $Password
-rabbitmqctl set_user_tags $User administrator
-rabbitmqctl set_permissions -p / $User ".*" ".*" ".*"
-
-#rabbitmq-plugins enable rabbitmq_web_stomp
-#sudo service rabbitmq-server restart
